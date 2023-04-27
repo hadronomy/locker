c27b42026c4d2ea81e4c18d7148ae642a18e24c6
Pablo Hernández <17086478+Hadronomy@users.noreply.github.com>
2023-04-27 13:46:53 +0100

    refactor: migrate to Next 13 `app/` directory
    Add clerk environment variables to `.env.example`.
    
    Run `prettier`.
    
    Closes: #3

--- src/env.mjs
+++ src/env.mjs
@@ -5,8 +5,9 @@ import { z } from 'zod';
  * built with invalid env vars.
  */
 const server = z.object({
-  DATABASE_URL: z.string().url(),
-  NODE_ENV: z.enum(['development', 'test', 'production'])
+  DATABASE_URL: z.string(),
+  NODE_ENV: z.enum(['development', 'test', 'production']),
+  CLERK_SECRET_KEY: z.string()
 });
 
 /**
@@ -14,11 +15,9 @@ const server = z.object({
  * built with invalid env vars. To expose them to the client, prefix them with `NEXT_PUBLIC_`.
  */
 const client = z.object(
-  /** @satisfies {Record<`NEXT_PUBLIC_${string}`, import('zod').ZodType>} */ (
-    {
-      // NEXT_PUBLIC_CLIENTVAR: z.string().min(1),
-    }
-  )
+  /** @satisfies {Record<`NEXT_PUBLIC_${string}`, import('zod').ZodType>} */ ({
+    NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: z.string().min(1)
+  })
 );
 
 /**
@@ -29,8 +28,10 @@ const client = z.object(
  */
 const processEnv = {
   DATABASE_URL: process.env.DATABASE_URL,
-  NODE_ENV: process.env.NODE_ENV
-  // NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
+  NODE_ENV: process.env.NODE_ENV,
+  NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY:
+    process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
+  CLERK_SECRET_KEY: process.env.CLERK_SECRET_KEY
 };
 
 // Don't touch the part below
