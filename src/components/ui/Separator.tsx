5b34d643881c85f51a9c0880fa9e8014ee58ad43
Pablo Hernández <17086478+Hadronomy@users.noreply.github.com>
2023-05-01 18:36:35 +0100

    feat: add skew variant to `<Separator>`

--- src/components/ui/Separator.tsx
+++ src/components/ui/Separator.tsx
@@ -10,21 +10,30 @@ export const separatorStyle = cva('bg-accent mx-[15px]', {
     orientation: {
       horizontal: 'h-px w-full',
       vertical: 'h-full w-px'
+    },
+    skew: {
+      none: '',
+      left: '-rotate-[20deg]',
+      right: 'rotate-[20deg]'
     }
+  },
+  defaultVariants: {
+    skew: 'none'
   }
 });
 
-export type SeparatorProps = RadixSeparator.SeparatorProps &
-  VariantProps<typeof separatorStyle>;
+export type SeparatorProps = VariantProps<typeof separatorStyle> &
+  RadixSeparator.SeparatorProps;
 
 export function Separator({
   className,
   orientation,
+  skew,
   ...props
 }: SeparatorProps) {
   return (
     <RadixSeparator.Root
-      className={cn(separatorStyle({ className, orientation }))}
+      className={cn(separatorStyle({ className, orientation, skew }))}
       orientation={orientation}
       {...props}
     />
