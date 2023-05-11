9b4695bc2f7faf0c0c0e7c9fc0714e117e42e3e2
Pablo Hernández <17086478+Hadronomy@users.noreply.github.com>
2023-05-11 17:44:46 +0100

    feat: add modal dialog to `Add New` button

--- src/app/panel/page.tsx
+++ src/app/panel/page.tsx
@@ -1,7 +1,20 @@
 import { Lock, Unlock } from 'lucide-react';
 
-import { Button } from '~/components/ui/Button';
+import { Button, buttonVariants } from '~/components/ui/Button';
 import { Input } from '~/components/ui/Input';
+import {
+  AlertDialog,
+  AlertDialogTrigger,
+  AlertDialogAction,
+  AlertDialogContent,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogCancel
+} from '~/components/ui/AlertDialog';
+import { cn } from '~/lib/utils';
+import { Label } from '~/components/ui/Label';
 
 export const metadata = {
   title: 'Locker - Panel'
@@ -30,9 +43,39 @@ export default function PanelPage() {
     <main className="mx-auto flex min-h-screen max-w-screen-xl flex-col px-5 md:px-10">
       <div className="mt-10 flex gap-x-3">
         <Input placeholder="Search..." accept="text" disabled />
-        <Button className="w-40 font-extrabold tracking-tighter">
-          Add New
-        </Button>
+        <AlertDialog>
+          <AlertDialogTrigger
+            className={`${cn(
+              buttonVariants(),
+              'w-40 font-extrabold tracking-tighter'
+            )}`}
+          >
+            Add New
+          </AlertDialogTrigger>
+          <AlertDialogContent>
+            <AlertDialogHeader>
+              <AlertDialogTitle>Add a New Lock</AlertDialogTitle>
+              <AlertDialogDescription>
+                <div className="grid w-full max-w-sm items-center gap-1.5">
+                  <Label htmlFor="name">Name</Label>
+                  <Input type="name" id="name" placeholder="Lock Name" />
+                </div>
+                <div className="mt-4 grid w-full max-w-sm items-center gap-1.5">
+                  <Label htmlFor="description">Description</Label>
+                  <Input
+                    type="description"
+                    id="description"
+                    placeholder="Description"
+                  />
+                </div>
+              </AlertDialogDescription>
+            </AlertDialogHeader>
+            <AlertDialogFooter>
+              <AlertDialogCancel>Cancel</AlertDialogCancel>
+              <AlertDialogAction>Continue</AlertDialogAction>
+            </AlertDialogFooter>
+          </AlertDialogContent>
+        </AlertDialog>
       </div>
       <div className="flex flex-col py-8">
         <div className="grid grid-cols-1 gap-8 md:grid-cols-3">
