7cb48eae5aa0e70edfff589a9b82b38678921ff8
Pablo Hernández <17086478+Hadronomy@users.noreply.github.com>
2023-07-23 13:45:04 +0100

    fix: add error boundary

--- src/app/panel/page.tsx
+++ src/app/panel/page.tsx
@@ -1,4 +1,6 @@
 import * as React from 'react';
+import { ErrorBoundary } from 'react-error-boundary';
+
 import { LockActionBar } from '~/components/panel/lock-action-bar';
 import { LockGrid } from '~/components/panel/lock-grid';
 
@@ -11,9 +13,11 @@ export default function PanelPage() {
     <main className="mx-auto flex min-h-screen max-w-screen-xl flex-col px-5 md:px-10">
       <LockActionBar />
       <div className="flex flex-col py-8">
-        <React.Suspense fallback={<p>Loading...</p>}>
-          <LockGrid />
-        </React.Suspense>
+        <ErrorBoundary fallback={<p>Oops!</p>}>
+          <React.Suspense fallback={<p>Loading...</p>}>
+            <LockGrid />
+          </React.Suspense>
+        </ErrorBoundary>
       </div>
     </main>
   );
